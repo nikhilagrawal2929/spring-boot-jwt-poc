c42e5ebc2d6e6ff71f80bc5437ea18994fcf4ac0
nikhilagrawal2929 <150434572+nikhilagrawal2929@users.noreply.github.com>
2023-12-01 18:25:23 +0530

    Update CryptoExample.java

--- CryptoExample.java
+++ CryptoExample.java
@@ -1,99 +1,45 @@
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
+import org.springframework.stereotype.Component;
 
-public class CryptoExample {
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-    public static void main(String[] args) throws Exception {
-        String encryptedText = "your_encrypted_string_here";
-        String key = "your_key_here";
+@Component
+public class CustomHeaderFilter implements Filter {
 
-        // Decode the Base64-encoded key
-        byte[] decodedKey = Base64.getDecoder().decode(key);
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Accept", "application/json");
+        httpServletResponse.setHeader("Content-Type", "application/json");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_NAME);
+        httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+        httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self';");
+        httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        httpServletResponse.setHeader("X-Frame-Options", "DENY");
+        httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpServletResponse.setHeader("Referrer-Policy", "strict-origin");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setHeader("Cache-Control", "no-cache");
+        httpServletResponse.setHeader("Permissions-Policy", "geolocation=(), midi=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), fullscreen=self, payment=0");
 
-        // Create a SecretKeySpec from the key
-        SecretKeySpec secretKeySpec = new SecretKeySpec(decodedKey, "AES");
-
-        // Initialize the Cipher with the key and the algorithm
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-
-        // Decode the Base64-encoded encrypted text
-        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
-
-        // Decrypt the text
-        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-
-        // Convert the decrypted bytes to a String
-        String decryptedText = new String(decryptedBytes);
-
-        System.out.println("Decrypted Text: " + decryptedText);
+        chain.doFilter(request, response);
     }
-    private String aesDecrypt(String aesEncrypt, String secretKey) throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
-		//String secretKey = "aesSecureSecretKey";
-		//String cipherText = "U2FsdGVkX1+tsmZvCEFa/iGeSA0K7gvgs9KXeZKwbCDNCs2zPo+BXjvKYLrJutMK+hxTwl/hyaQLOaD7LLIRo2I5fyeRMPnroo6k8N9uwKk=";
- 
-		byte[] cipherData = Base64.getDecoder().decode(aesEncrypt);
-		byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
- 
-		MessageDigest md5 = MessageDigest.getInstance("MD5");
-		final byte[][] keyAndIV = GenerateKeyAndIV(32, 16, 1, saltData, secretKey.getBytes(StandardCharsets.UTF_8), md5);
-		SecretKeySpec key = new SecretKeySpec(keyAndIV[0], "AES");
-		IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
- 
-		byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
-		Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
-		byte[] decryptedData = aesCBC.doFinal(encrypted);
-		String decryptedText = new String(decryptedData, StandardCharsets.UTF_8);
- 
-		System.out.println(decryptedText);
-		return decryptedText;
-	}
-	public byte[][] GenerateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password, MessageDigest md) {
- 
-	    int digestLength = md.getDigestLength();
-	    int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
-	    byte[] generatedData = new byte[requiredLength];
-	    int generatedLength = 0;
- 
-	    try {
-	        md.reset();
- 
-	        // Repeat process until sufficient data has been generated
-	        while (generatedLength < keyLength + ivLength) {
- 
-	            // Digest data (last digest if available, password data, salt if available)
-	            if (generatedLength > 0)
-	                md.update(generatedData, generatedLength - digestLength, digestLength);
-	            md.update(password);
-	            if (salt != null)
-	                md.update(salt, 0, 8);
-	            md.digest(generatedData, generatedLength, digestLength);
- 
-	            // additional rounds
-	            for (int i = 1; i < iterations; i++) {
-	                md.update(generatedData, generatedLength, digestLength);
-	                md.digest(generatedData, generatedLength, digestLength);
-	            }
- 
-	            generatedLength += digestLength;
-	        }
- 
-	        // Copy key and IV into separate byte arrays
-	        byte[][] result = new byte[2][];
-	        result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
-	        if (ivLength > 0)
-	            result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
- 
-	        return result;
- 
-	    } catch (DigestException e) {
-	        throw new RuntimeException(e);
- 
-	    } finally {
-	        // Clean out temporary data
-	        Arrays.fill(generatedData, (byte)0);
-	    }
-	}
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code if needed
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code if needed
+    }
 }
