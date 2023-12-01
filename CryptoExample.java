37dacfb9dfe80c66bf320f572c029e37d890e191
nikhilagrawal2929 <150434572+nikhilagrawal2929@users.noreply.github.com>
2023-12-01 18:29:53 +0530

    Update CryptoExample.java

--- CryptoExample.java
+++ CryptoExample.java
@@ -1,45 +1,62 @@
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class CustomHeaderFilter implements Filter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Accept", "application/json");
-        httpServletResponse.setHeader("Content-Type", "application/json");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_NAME);
-        httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-        httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self';");
-        httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
-        httpServletResponse.setHeader("X-Frame-Options", "DENY");
-        httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
-        httpServletResponse.setHeader("Referrer-Policy", "strict-origin");
-        httpServletResponse.setHeader("Pragma", "no-cache");
-        httpServletResponse.setHeader("Cache-Control", "no-cache");
-        httpServletResponse.setHeader("Permissions-Policy", "geolocation=(), midi=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), fullscreen=self, payment=0");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll()
+                .and()
+            .httpBasic();
 
-        chain.doFilter(request, response);
+        // Add your custom headers
+        http.headers()
+                .httpStrictTransportSecurity()
+                    .includeSubDomains(true)
+                    .maxAgeInSeconds(31536000)
+                    .preload(true)
+                    .and()
+                .contentSecurityPolicy("default-src 'self';")
+                .xssProtection()
+                    .block(true)
+                    .and()
+                .frameOptions()
+                    .deny()
+                    .and()
+                .contentTypeOptions()
+                    .nosniff()
+                    .and()
+                .referrerPolicy()
+                    .strictOrigin()
+                    .and()
+                .pragma()
+                    .noCache()
+                    .and()
+                .cacheControl()
+                    .noCache()
+                    .and()
+                .addHeaderWriter((request, response) -> {
+                    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+                    response.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_NAME);
+                    response.setHeader("Permissions-Policy", "geolocation=(), midi=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), fullscreen=self, payment=0");
+                });
     }
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Initialization code if needed
-    }
-
-    @Override
-    public void destroy() {
-        // Cleanup code if needed
-    }
+    // Other configurations or beans if needed
 }
